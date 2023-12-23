73d51470d4e420a7277920c427fb72bf724d351b
Aashish Katila <aashishkatila@gmail.com>
2023-12-23 19:22:36 +0545

    HTML and CSS done

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,49 +1,39 @@
-import React, { useState } from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  const [value, setValue] = useState('');
-
+const App = () => {
   return (
-    <div className="container">
-      <div className="calculator">
-        <form action="">
-          <div className='display'>
-            <input type= "text" value={value}/>
-            </div>
-          <div>
-            <input type="button" value="AC" onClick={e => setValue('')}/>
-            <input type="button" value="DE" onClick={e => setValue(value.slice(0, -1))}/>
-            <input type="button" value="." onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="/" onClick={e => setValue(value + e.target.value)}/>
-          </div>
-          <div>
-            <input type="button" value="7" onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="8" onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="9" onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="*" onClick={e => setValue(value + e.target.value)}/>
-          </div>
-          <div>
-            <input type="button" value="4" onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="5" onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="6" onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="+" onClick={e => setValue(value + e.target.value)}/>
-          </div>
-          <div>
-            <input type="button" value="1" onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="2" onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="3" onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="-" onClick={e => setValue(value + e.target.value)}/>
-          </div>
-          <div>
-            <input type="button" value="00" onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="0" onClick={e => setValue(value + e.target.value)}/>
-            <input type="button" value="=" className='equal' onClick={e => setValue(eval(value))}/>
-          </div>
-        </form>
+    <div className='calculator-grid'>
+      
+      <div className="output">
+        <div className="previous-operand">123*</div>
+        <div className="current-operand">321</div>
       </div>
+      
+      <button className='span-two'>AC</button>
+      <button >DEL</button>
+      <button >/</button>
+      
+      <button >1</button>
+      <button >2</button>
+      <button >3</button>
+      <button >*</button>
+      
+      <button >4</button>
+      <button >5</button>
+      <button >6</button>
+      <button >+</button>
+
+      <button >7</button>
+      <button >8</button>
+      <button >9</button>
+      <button >-</button>
+
+      <button >.</button>
+      <button >0</button>
+      <button className='span-two' >=</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
